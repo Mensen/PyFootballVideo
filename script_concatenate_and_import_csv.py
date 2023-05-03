8e6ce2ff67fab877f997394d20323e438ae687da
Armand Mensen <Mensen@users.noreply.github.com>
2023-05-04 01:11:38 +0200

    removed dependency on ffmpeg-python and added a csv merge function

--- script_concatenate_and_import_csv.py
+++ script_concatenate_and_import_csv.py
@@ -5,26 +5,31 @@ import os
 import glob
 import csv
 import subprocess
-# nice wrapper for direct ffmpeg functions (must have ffmpeg installed directly)
-import ffmpeg
-import time # just for timing processes and testing
+import re                       # regular expressions
+import pandas as pd
 
 # --------------------------- #
+def define_paths_breakdown():
+    base_path = r"F:\_In Processing"
+    # on my laptop
+    base_path = r"C:\Users\mense\AFC Zurich Renegades\Renegades General - Documents\Game_Film"
+    # on my desktop
+    base_path = r"F:\AFC Zurich Renegades\Renegades General - Documents\Game_Film"
+    game_path = r"Gameday 04302023\NLA ZRvsGS"
+    specific_path = r"Full game zoomed"
+    working_path = os.path.join(base_path, game_path, specific_path)
 
-# define working path
-# base_path = r"E:\one_drive\Bern Grizzlies\Coaches - Documents\NLA Game Film"
-# specific_path = r"Game 5 - Calanda\Sony"
-
-base_path = r"F:\_In Processing"
-game_path = r"Game 10 - Zurich"
-specific_path = r"GP9\IndividualClips"
-working_path = os.path.join(base_path, game_path, specific_path)
-
+    # Check existence
+    if os.path.exists(working_path) == False:
+        print("Error - File: " + working_path + " doesn't exist")
+    else:
+        print("Found it! " + working_path)
 
 
 def make_filelist(working_path):
     # get all the mp4 files (with "Clip" in the name)
-    file_list = glob.glob(working_path + "**/*Clip*.mp4", recursive=True)
+    file_list = glob.glob(working_path + "**/*DJI*.mp4", recursive=True)
+    len(file_list)
 
     # write list as text file for later reference with concat
     with open(os.path.join(working_path, "mp4_list.txt"), "w") as output:
@@ -32,17 +37,34 @@ def make_filelist(working_path):
 
 
 def get_video_duration(file_list):
+
+    clip_times_path = os.path.join(working_path, "clip_times.csv")
+
+    # is ffmpeg-python installed?
+    flag_ffmpeg = 0
+
     # loop over each file and get its duration
     video_duration = []
     video_name = []
     for file in file_list:
 
-        video_name.append("play "+ str(file_list.index(file)+1))
-        video_info = ffmpeg.probe(file)
-        video_duration.append(float(video_info['streams'][0]['duration'])) # in seconds
+        video_name.append("clip "+ str(file_list.index(file)+1))
 
-    # check video length    
-    # video_duration.__len__()
+        if flag_ffmpeg == 1:
+            video_info = ffmpeg.probe(file)
+            video_duration.append(float(video_info['streams'][0]['duration'])) # in seconds
+
+        else:
+            # call ffprobe on the command line to find duration          
+            ffprobe_cmd = ["ffprobe", 
+                "-v", "error", 
+                "-show_entries", "format=duration", 
+                "-of", "default=noprint_wrappers=1:nokey=1", 
+                "-i", file]
+            output = subprocess.check_output(ffprobe_cmd, stderr=subprocess.STDOUT)
+
+            # Convert the duration string to a float
+            video_duration.append(float(output))
 
     # add durations to get each clip start time
     video_starttime = []
@@ -51,12 +73,16 @@ def get_video_duration(file_list):
         video_starttime.append(video_starttime[item] + video_duration[item])
 
     # write start time (i.e. clip position) and duration to csv file for DF import
-    rows = zip(video_name, video_starttime, video_duration)
-    with open(os.path.join(working_path, (game_path + " - clip_times.csv")), 'w', newline='') as f:
+    # combine the lists into a list of tuples
+    rows = list(zip(video_name, video_starttime, video_duration))
+    # open the output CSV file and write the data
+    with open(clip_times_path, 'w', newline='') as csv_file:
         
         # using csv.writer method from CSV package
-        write = csv.writer(f)
+        write = csv.writer(csv_file)
+        # write the header row
         write.writerow(['Name','Position', 'Duration'])
+
         for row in rows:
             write.writerow(row)
 
@@ -64,35 +90,42 @@ def get_video_duration(file_list):
 def concatenate_video(working_path):
     # concatenate the video
 
-    #https://github.com/kkroening/ffmpeg-python/issues/137
-    (
-        ffmpeg
-        .input(os.path.join(working_path, "mp4_list.txt"), format='concat', safe=0)
-        .output(os.path.join(working_path, 'concatenated_video.mp4'), map='0:0', vcodec = "copy")
-        .run()
-    )
+    # file with list of clips
+    clips_file_path = os.path.join(working_path, "mp4_list.txt")
+    output_file_path = os.path.join(working_path, "combined_clips.mp4")
 
-    # ffmpeg -f concat -safe 0 -i mp4_list.txt -c copy Game6WW_GP8.mp4
-    (
-        ffmpeg
-        .input(os.path.join(working_path, "mp4_list.txt"), format='concat', safe=0)
-        .output(os.path.join(working_path, 'concatenated_video.mp4'), vcodec = "copy")
-        .run()
-    )
+    # Create a command string to concatenate the videos using ffmpeg
+    ffmpeg_cmd = (
+        'ffmpeg -hide_banner -loglevel error -y '
+        '-f concat -safe 0 -i \"{}\" '
+        '-map 0:v -c:v copy '
+        '\"{}\"'
+    ).format(clips_file_path, output_file_path)
 
-    # or call ffmpeg directly (might be faster, still to be tested)
-    # ffmpeg -f concat -safe 0 -i temp_files.txt -c copy concatenated_video.mp4
+    subprocess.run(ffmpeg_cmd, shell=True)
 
-    output_name = '"' + os.path.join(working_path, (game_path + " - Clipped Test GP9.mp4")) + '"'
-    cmd = ["ffmpeg " +
-    "-f concat " +
-    "-safe 0 " +
-    "-i " + '"' + os.path.join(working_path, "mp4_list.txt") + '" '
-    "-map 0:v " +
-    "-vcodec copy " +
-    "-hide_banner " +
-    output_name] 
-    subprocess.run(cmd[0], shell=True)
+
+def merge_df_hudl():
+
+    # save the xlsx file from hudl to csv manually (otherwise missing dependencies in pandas)
+    hudl_export_path = r"Gameday 04302023\NLA ZRvsGS\hudl_export.csv"
+    hudl_export_path = os.path.join(base_path, hudl_export_path)
+
+    output_file_path = os.path.join(base_path, r"Gameday 04302023\NLA ZRvsGS\ready4DF.csv")
+
+    # read in the csv and excel files as pandas dataframes
+    clip_times = pd.read_csv(clip_times_path)
+    hudl_export = pd.read_csv(hudl_export_path)
+
+    # check that they have the same number of rows
+    if len(clip_times) == len(hudl_export):
+        # merge the two dataframes horizontally (i.e. add columns to the right)
+        merged_df = pd.concat([clip_times, hudl_export], axis=1)
+        
+        # save the merged dataframe as a csv file
+        merged_df.to_csv(output_file_path, index=False)
+    else:
+        print("Error: clip_times.csv and hudl_export.xlsx have different number of rows.")
 
 
 def recode_video(working_path):
